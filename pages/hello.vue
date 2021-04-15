b4d2cb425d29dc3c8cf6a5bd006203e62b0709b5
Yutaka Catch <961566+ycatch@users.noreply.github.com>
2021-04-15 20:41:39 +0900

    1st commit

---
+++ pages/hello.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-row>
+    <v-col class="text-center">
+      <img src="/v.png" alt="Vuetify.js" class="mb-5" />
+      <blockquote class="blockquote">
+        Hello world
+        <footer>
+          <small>
+            <em>&mdash; Yutaka Kachi</em>
+          </small>
+        </footer>
+      </blockquote>
+    </v-col>
+  </v-row>
+</template>
